841026bc55c30d92849d97ab85b3bcdce519c57b
Riya_dhrangadiya <dhrangadiyariya@gmail.com>
2026-01-10 09:25:13 +0530

    Add files via upload

---
+++ asse.py
@@ -0,0 +1,48 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("hello world")
+hello world
+a=int(input("enter frist num:"))
+enter frist num:5
+b=int(input("enter second num:"))
+enter second num:5
+sum=a
+sum=a+b
+print("sum=",sum)
+sum= 10
+num=int(input("enter a num:"))
+enter a num:4
+if num % 2==0:
+    print("even num")
+else:
+    print("odd num")
+print("enter num=",num)
+SyntaxError: invalid syntax
+import math
+print("PI value=",math.pi)
+PI value= 3.141592653589793
+MAX_VALUE= 100
+print(MAX_VALUE)
+100
+num-int(input("enter a num:"))
+enter a num:3
+1
+num=int(input("enter a num:"))
+enter a num:5
+square=num*num
+print("square=",square)
+square= 25
+r=float(input("enter radius:"))
+enter radius:2
+area= 3.14*r*r
+print("area of circle=",area)
+area of circle= 12.56
+x=10
+print(type(x))
+<class 'int'>
+y=3.14
+print(type(y))
+<class 'float'>
+import math
+print(math.sqrt(25))
+5.0
