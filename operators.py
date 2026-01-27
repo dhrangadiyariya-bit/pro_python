56122f92eb41d589d9db6a738755c89db0c18504
Riya_dhrangadiya <dhrangadiyariya@gmail.com>
2026-01-27 08:24:56 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,71 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:01:55) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=5
+>>> b=10
+>>> print(a+b)
+15
+>>> print(a-b)
+-5
+>>> print(a/b)
+0.5
+>>> 
+>>> 
+>>> #assignment operator
+>>> x=10
+>>> print(x)
+10
+>>> #unary minus
+>>> a=5
+>>> print(-a)
+-5
+>>> #relational operator
+>>> p=10
+>>> q=5
+>>> print(p>q)
+True
+>>> print(p==q)
+False
+>>> #logical operator
+>>> x=True
+>>> y=False
+>>> print(x and y)
+False
+>>> print(y or y)
+False
+>>> print(not x)
+False
+>>> #boolean
+>>> is-pass=True
+SyntaxError: invalid syntax
+>>> is_pass=True
+>>> print(is_pass)
+True
+>>> #bitwise operator
+>>> a=5
+>>> b=3
+>>> print(a*b)
+15
+>>> print(a/b)
+1.6666666666666667
+>>> print(a&b)
+1
+>>> #membership operator
+>>> numbers=(1,2,3,4)
+>>> print(2 in number )
+Traceback (most recent call last):
+  File "<pyshell#36>", line 1, in <module>
+    print(2 in number )
+NameError: name 'number' is not defined
+>>> x=10
+>>> y=10
+>>> print(x is y)
+True
+>>> print(x is not y)
+False
+>>> #membership opetaror
+>>> numbers=(1,2,3,4)
+>>> print(2 in numbers)
+True
+>>> print(5 is not numbers)
+True
+>>> 
